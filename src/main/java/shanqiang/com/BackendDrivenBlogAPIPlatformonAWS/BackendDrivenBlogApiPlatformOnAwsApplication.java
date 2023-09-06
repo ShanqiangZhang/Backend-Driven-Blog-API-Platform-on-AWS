37cc6f32eb76b05c5ffb63aeac6017e9127c2730
Shanqiang <zhang.shanq@northeastern.edu>
2023-09-06 14:07:40 -0700

    initialize API project

---
+++ src/main/java/shanqiang/com/BackendDrivenBlogAPIPlatformonAWS/BackendDrivenBlogApiPlatformOnAwsApplication.java
@@ -0,0 +1,13 @@
+package shanqiang.com.BackendDrivenBlogAPIPlatformonAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDrivenBlogApiPlatformOnAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDrivenBlogApiPlatformOnAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shanqiang/com/BackendDrivenBlogAPIPlatformonAWS/BackendDrivenBlogApiPlatformOnAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package shanqiang.com.BackendDrivenBlogAPIPlatformonAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDrivenBlogApiPlatformOnAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
