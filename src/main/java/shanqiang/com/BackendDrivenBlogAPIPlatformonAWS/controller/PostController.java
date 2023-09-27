e55af5d36519cc43dd770e562d96de51107dd1b8
Shanqiang <zhang.shanq@northeastern.edu>
2023-09-27 09:15:06 -0700

    add role authentication test

--- src/main/java/shanqiang/com/BackendDrivenBlogAPIPlatformonAWS/controller/PostController.java
+++ src/main/java/shanqiang/com/BackendDrivenBlogAPIPlatformonAWS/controller/PostController.java
@@ -3,6 +3,7 @@ package shanqiang.com.BackendDrivenBlogAPIPlatformonAWS.controller;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import shanqiang.com.BackendDrivenBlogAPIPlatformonAWS.payload.PostDto;
 import shanqiang.com.BackendDrivenBlogAPIPlatformonAWS.payload.PostResponse;
@@ -21,6 +22,7 @@ public class PostController {
     }
 
     //create blog post
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -42,11 +44,14 @@ public class PostController {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePostById(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
         PostDto postResponse = postService.updatePostById(postDto, id);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePostById(@PathVariable(name = "id") long id) {
